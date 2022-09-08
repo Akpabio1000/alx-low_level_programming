109ed875ab954f131d5835dd30c22bd85e30ebe3
Akpabio Emmanuel <akpabio15@gmail.com>
2022-09-08 16:56:31 -0700

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,7 @@
 
 /**
  * main - A program that prints sizeof types
+ *
  * Return - return 0
  */
 
@@ -10,8 +11,8 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of a int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %llu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %Lf byte(s)\n", sizeof(float));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %f byte(s)\n", sizeof(float));
 
 	return (0);
 }
