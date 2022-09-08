5f12f9d260f78c8ab736b625680b1044261a65c9
Akpabio Emmanuel <akpabio15@gmail.com>
2022-09-08 15:29:22 -0700

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - To demonstrate the puts keyword
+ * Return: return 0
+ */
+int main(void)
+{
+	char a[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(a);
+
+	return (0);
+}
+
