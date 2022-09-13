ec9465b10b5ac6dc8a288a54f8b932c3d229a4fc
Akpabio Emmanuel <akpabio15@gmail.com>
2022-09-13 15:29:42 -0700

    Add space before '('

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	char letter;
 	int i;
 
-	for(i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for(letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
