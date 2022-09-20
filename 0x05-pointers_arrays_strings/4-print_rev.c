1154dc0729f147f4d93acf83985eea1df85aec2d
Akpabio Emmanuel <akpabio15@gmail.com>
2022-09-19 19:47:53 -0700

    add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order
+ * @s: the string to be reversed
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	for (i = len ; i >= 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
