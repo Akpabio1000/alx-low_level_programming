e67d935b486f027e9491e354b1c48560041ac511
Akpabio Emmanuel <akpabio15@gmail.com>
2022-09-22 21:38:51 -0700

    correct char array

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,28 +9,20 @@
 
 char *rot13(char *s)
 {
-
-	int a[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-		     'W', 'X', 'Y', 'Z'};
-	int b[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-		     'J', 'K', 'L', 'M'};
-
 	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char n[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (j = 0; s[j] != '\0'; j++) 
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		i = 0;
-		while (a[i] != '\0' && s[j] != a[i])
-			i++;
-
-		if (s[j] == a[i]) 
-			s[j] = b[i];
+		for (j = 0; j < 56; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = n[j];
+				break;
+			}
+		}
 	}
 
 	return (s);
