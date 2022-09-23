ac0abbaa7e6d68558e4d525da0d097b0db2fa94b
Akpabio Emmanuel <akpabio15@gmail.com>
2022-09-22 21:29:13 -0700

    update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,19 @@
 
 char *leet(char *s)
 {
-
-	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	
+	char arr[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char replace[] = {'4', '3', '0', '7', '1'};
 	int i, j;
 
-	for (j = 0; s[j] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; a[i] != '\0'; i++)
+		for (j = 0; arr[j] != '\0'; j++)
 		{
-			if (s[j] == a[i])
-				s[j] = b[i];
+			if (s[i] == arr[j])
+				s[i] = replace[j / 2];
 		}
 	}
-
 	return (s);
 }
+
+
