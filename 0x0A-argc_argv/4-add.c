c739a1da743c41da2f6d1504edcc3cac460db568
Akpabio Emmanuel <akpabio15@gmail.com>
2022-09-30 16:18:21 -0700

    Re-fix negative numbers summation

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,66 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* atoi */
+#include <stdbool.h> /* bool data type */
 
 /**
- * is_digit - checks if each character is a digit
- * @str: string to check
- * Return: 1 if all characters are digits, otherwise 0
+ * is_num - iterate through each argv to test if it's a number
+ * @argvv: a argv
+ * Return: true only if entire string is a number, false if not
  */
 
-int is_digit(char *str)
+bool is_num(char *argvv)
 {
-	int i = 0;
+	int j = 0;
 
-	for (i = 0; str[i]; i++)
+	for (j = 0; argvv[j]; j++)
 	{
-		if (str[i] == '-')
-			continue;
-		if (!(str[i] >= '0' && str[i] <= '9'))
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
 			return (0);
 	}
 	return (1);
 }
 
 /**
-* main - adds numbers
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: returns 0 if no number is given; Error if a non-digit is given,
-* returning 1; otherwise, prints sum and return 0.
-*/
+ * main - print sum if all arguments given are numbers
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on success, 1 if an argument wasn't a number
+ */
 
 int main(int argc, char *argv[])
 {
+	int i = 1;
 	int sum = 0;
-	int i;
 
-	/* argc is 1 if only the program name is inputted */
-
+	/* validate input */
 	if (argc == 1)
 	{
-		printf("%d\n", sum);
+		printf("0\n");
 		return (0);
 	}
 
-	/**
-	 * loop through argv, passing each argument
-	 * as s string to is_digit.
-	 * is_digit checks each character present in the 
-	 * string passed
-	 */
-
-	for (i = 1; i < argc; i++)
+	/* check all arguments to add numbers */
+	while (i < argc)
 	{
-		if (is_digit(argv[i]))
+		if (is_num(argv[i]))
 			sum += atoi(argv[i]);
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
+		i++;
 	}
 	printf("%d\n", sum);
+
 	return (0);
 }
-
