c6a3250f25dff41bb185167ae8efcf5a3f4bdf7e
Akpabio Emmanuel <akpabio15@gmail.com>
2022-10-04 05:02:08 -0700

    Add 0-create_array.c amd main.h

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: size of array
+ * @c: character to initialize array with.
+ * Return: NULL if size is 0, or a pointer to the array on success
+ * or NULL if the returned pointer fails.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = (char *)malloc(size);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
+
+
+
