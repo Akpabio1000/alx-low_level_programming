efea68c3c5104769067fe889841c93827b4d509f
Akpabio Emmanuel <akpabio15@gmail.com>
2022-10-04 08:14:22 -0700

    fix array size < 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,10 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
-	if (size == 0)
+	if (size <= 0)
 		return (NULL);
 
-	array = (char *)malloc(size);
+	array = (char *)malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
