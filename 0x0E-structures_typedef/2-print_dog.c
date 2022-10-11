949705a0ad0de7c07f7da248f8ac50142241a8cf
Akpabio Emmanuel <akpabio15@gmail.com>
2022-10-10 21:15:12 -0700

    Add 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog variable
+ */
+
+void print_dog(struct dog *d)
+{
+
+	(d->name == NULL) ? printf("Name: (nil)\n")
+		: printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	(d->owner == NULL) ? printf("Owner: (nil)\n")
+		: printf("Owner: %s\n", d->owner);
+
+}
